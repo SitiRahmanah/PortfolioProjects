cded272fcc4f4d083412b1e5cdf0dbcf8a88ddec
Siti Rahmanah <cty.siti@gmail.com>
2023-03-02 11:15:25 +0800

    add nashville housing data cleaning file

---
+++ Data Cleaning - Nashville Housing.sql
@@ -0,0 +1,25 @@
+/* Cleaning Data */
+
+select *
+from PortfolioProject.dbo.NashvilleHousing
+
+-- Standardize Date Format
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+ALTER COLUMN SaleDate Date
+
+select *
+from PortfolioProject.dbo.NashvilleHousing
+
+-- Populate Property Address
+
+select *
+from PortfolioProject.dbo.NashvilleHousing
+--where PropertyAddress is NULL
+order by ParcelID
+
+select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress
+from PortfolioProject.dbo.NashvilleHousing a
+JOIN PortfolioProject.dbo.NashvilleHousing b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ]<>b.[UniqueID ]
