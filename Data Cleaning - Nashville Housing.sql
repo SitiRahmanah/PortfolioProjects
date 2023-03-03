73c735e2bf550485ab8ad44b8127a229a1a0e8c9
Siti Rahmanah <cty.siti@gmail.com>
2023-03-03 09:52:40 +0800

    breaking out address data

--- Data Cleaning - Nashville Housing.sql
+++ Data Cleaning - Nashville Housing.sql
@@ -31,3 +31,28 @@ JOIN PortfolioProject.dbo.NashvilleHousing b
 	ON a.ParcelID = b.ParcelID
 	AND a.[UniqueID ] <> b.[UniqueID ]
 where a.PropertyAddress is NULL
+
+
+-- Breaking out address into individual columns (Address, City, State)
+
+select PropertyAddress
+from PortfolioProject.dbo.NashvilleHousing
+
+select SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1) as Address,
+SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, LEN(PropertyAddress)) as City
+from PortfolioProject.dbo.NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+ADD PropertySplitAddress nvarchar(255);
+
+UPDATE NashvilleHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1);
+
+ALTER TABLE NashvilleHousing
+ADD PropertySplitCity nvarchar(255);
+
+UPDATE NashvilleHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, LEN(PropertyAddress))
+
+select *
+from PortfolioProject.dbo.NashvilleHousing
